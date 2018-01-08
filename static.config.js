4fe932fdcba02b9b75d3a056fc6377614f71e8a9
loganpowell <loganp@tepper.cmu.edu>
2018-01-08 09:38:08 -0500

    trying a full branch instead of using dist

--- static.config.js
+++ static.config.js
@@ -154,7 +154,7 @@ export default {
               loader: 'style-loader',
               options: {
                 hmr: false,
-                sourceMap: true,
+                sourceMap: false,
               },
             },
             use: [
@@ -163,7 +163,7 @@ export default {
                 options: {
                   importLoaders: 1,
                   minimize: true,
-                  sourceMap: true,
+                  sourceMap: false,
                 },
               },
               {
@@ -189,7 +189,7 @@ export default {
               {
                 loader: 'less-loader',
                 options: {
-                  sourceMap: true,
+                  sourceMap: false,
                   modifyVars: themeVariables,
                 },
               },
